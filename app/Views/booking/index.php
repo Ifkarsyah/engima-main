d750c7235a8dbe45a2cff0ab24e627bc60bb5efe
Ferdian Ifkarsyah <13517024@std.stei.itb.ac.id>
2019-09-23 10:30:35 +0700

    Use the data given from controller

--- app/Views/booking/index.php
+++ app/Views/booking/index.php
@@ -1,8 +1,31 @@
-<form action="<?php echo URL_BASE_PUBLIC; ?>">
-    <?php for ($i = 1 ; $i <= 10;$i++)
+<div class = "header">
+    <h1><?php $this->data['bookInfo']['movie']; ?></h1>
+    <h4><?php $this->data['bookInfo']['date_time']; ?><PM></PM></h4>
+</div>
+
+<form action="<?php echo URL_BASE_PUBLIC; ?>booking/book" method="post">
+
+    <div class="seats">
+        <?php for ($i = 1 ; $i <= 30;$i++)
         {
-            echo "<input type=\"radio\" name=\"seat\" value=$i>$i<br>";
+            if ($this->data['seats'][$i])
+            {
+                echo "<input type=\"radio\" name=\"seatNumber\" value=$i>$i : Udah di pesan<br>";
+            }
+            else
+            {
+                echo "<input type=\"radio\" name=\"seatNumber\" value=$i>$i : Bisa dipilih<br>";
+            }
+
         }
         ?>
+    </div>
+
+    <div class="booking-summary">
+
+    </div>
+
     <input type="submit" value="Buy Ticket">
+
 </form>
+
