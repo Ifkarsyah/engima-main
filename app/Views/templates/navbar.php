7774bf682015fab6fb6ccfd00e17b785726dac4c
Ferdian Ifkarsyah <13517024@std.stei.itb.ac.id>
2019-09-27 14:09:43 +0700

    Dynamically change navbar based on user login status

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,8 +1,8 @@
-<?php use App\Core\BaseController;
-
-if (isset(BaseController::$userLoggedIn)): ?>
-    <p>Engima. Search Tab. Transactions. </p>
-    <a href="<? URL_BASE_PUBLIC; ?>logout">Logout</a>
-<? else: ?>
-    <p>Engima. Search Tab. Transactions. Nyuruh Login</p>
-<?endif; ?>
+<div class="nav">
+    <ul>
+        <li><a class="active" href="<? echo URL_BASE_PUBLIC; ?>home""><engima><eng>Engi</eng>ma</engima></a></li>
+        <li><input type="text2" placeholder="Search movie" name="search2"></li>
+        <li style="float:right"><a href = "<? echo URL_BASE_PUBLIC; ?>logout""><navbar>Logout</navbar></a></li>
+        <li style="float:right"><a href = "<? echo URL_BASE_PUBLIC; ?>transaction""><navbar>Transaction</navbar></a></li>
+    </ul>
+</div>
