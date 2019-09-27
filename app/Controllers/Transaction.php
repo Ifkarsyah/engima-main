af56c4235b481a2de6872a8b19596806418a26fd
Ferdian Ifkarsyah <13517024@std.stei.itb.ac.id>
2019-09-28 01:19:55 +0700

    Add transaction index

--- app/Controllers/Transaction.php
+++ app/Controllers/Transaction.php
@@ -17,6 +17,23 @@ class Transaction extends BaseController
      */
     public function index()
     {
+        $userID = $this->getUserIDFromCookies();
 
+        $this->view->data['pageTitle'] = 'Search';
+        $this->view->data['movies'] = $this->useModel('Transaction')->getTransactionHistory($userID);
+
+        print_r($this->view->data);
+        $this->view->addCSS('css/index.css');
+        $this->view->addCSS('css/search.css');
+        $this->view->render('templates/header');
+        $this->view->render('templates/navbar');
+        $this->view->render('search/index');
+        $this->view->render('templates/footer');
+    }
+
+    public function deleteReview($transactionID)
+    {
+        $this->getUserIDFromCookies();
+        $this->useModel('Transaction')->deleteReview($transactionID);
     }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,13 +4,31 @@
 namespace App\Models;
 
 
-use App\Core\BaseController;
+use App\Core\BaseModel;
 
 /**
  * Class Transaction
  * @package App\Models
  */
-class Transaction extends BaseController
+class Transaction extends BaseModel
 {
+    public function getTransactionHistory($userID)
+    {
+        $result = $this->db->execute(
+            "SELECT t.id, m.title, m.poster, s.date_time 
+                         FROM transactions t JOIN schedules s ON t.schedule_id = s.id
+                                JOIN movies m on s.movie_id = m.id
+                         WHERE user_id = :userID",
+            ['userID' => $userID]
+        );
+        return $result->getQueryResult();
+    }
 
+    public function deleteReview($transactionID)
+    {
+        $this->db->execute(
+            "DELETE FROM reviews WHERE transaction_id = :transactionID",
+            ['transactionID' => $transactionID]
+        );
+    }
 }
