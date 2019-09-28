c28fec8345d4378c655ffddf5d7e651b7ef7e762
Winston Wijaya <13517018@std.stei.itb.ac.id>
2019-09-28 19:31:13 +0700

    add engima and search picture

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,8 +1,7 @@
-<div class="nav">
-    <ul>
-        <li><a class="active" href="<? echo URL_BASE_PUBLIC; ?>home""><engima><eng>Engi</eng>ma</engima></a></li>
-        <li><input type="text2" placeholder="Search movie" name="search2"></li>
-        <li style="float:right"><a href = "<? echo URL_BASE_PUBLIC; ?>logout""><navbar>Logout</navbar></a></li>
-        <li style="float:right"><a href = "<? echo URL_BASE_PUBLIC; ?>transaction""><navbar>Transaction</navbar></a></li>
-    </ul>
+<div class="nav-container">
+    <img id="engima" src="/tugas-besar-1-2019/public/img/navigation/engima.png" alt="engima" href="<? echo URL_BASE_PUBLIC; ?>home">
+    <input type="text2" placeholder="Search movie" name="search2">
+    <img id="s_icon" src= "/tugas-besar-1-2019/public/img/navigation/search.png" alt="search">
+    <div id="right_opt" href = "<? echo URL_BASE_PUBLIC; ?>logout">Logout</div>
+    <div id="tra" href = "<? echo URL_BASE_PUBLIC; ?>transaction">Transaction</div>
 </div>
