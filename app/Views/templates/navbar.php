afa9cb1f4c17687bbf1f5f9fa38ff6114af8127b
Winston Wijaya <13517018@std.stei.itb.ac.id>
2019-09-28 19:40:57 +0700

    refine navbar

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -1,7 +1,7 @@
 <div class="nav-container">
-    <img id="engima" src="/tugas-besar-1-2019/public/img/navigation/engima.png" alt="engima" href="<? echo URL_BASE_PUBLIC; ?>home">
+    <a href="<? echo URL_BASE_PUBLIC; ?>home"><img id="engima" src="/tugas-besar-1-2019/public/img/navigation/engima.png" alt="engima"></a>
     <input type="text2" placeholder="Search movie" name="search2">
     <img id="s_icon" src= "/tugas-besar-1-2019/public/img/navigation/search.png" alt="search">
-    <div id="right_opt" href = "<? echo URL_BASE_PUBLIC; ?>logout">Logout</div>
-    <div id="tra" href = "<? echo URL_BASE_PUBLIC; ?>transaction">Transaction</div>
+    <a href = "<? echo URL_BASE_PUBLIC; ?>logout"><div id="right_opt">Logout</div></a>
+    <a href = "<? echo URL_BASE_PUBLIC; ?>transaction"><div id="tra" >Transaction</div></a>
 </div>
