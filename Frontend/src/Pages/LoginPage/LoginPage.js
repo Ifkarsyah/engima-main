f30ecccafbb97494c20712f78642de935206e69d
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-11-18 19:29:18 +0700

    Fix size General Form

--- Frontend/src/Pages/LoginPage/LoginPage.js
+++ Frontend/src/Pages/LoginPage/LoginPage.js
@@ -1,46 +1,62 @@
-import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { Redirect } from "react-router";
-import Authentication from "../../Utilities/Auth";
+import React from "react";
+import {Button, Form} from "react-bootstrap";
+import useForm from "react-hook-form";
+import {Link} from "react-router-dom";
 
 function LoginForm() {
-  const [accountNumber, setAccountNumber] = useState("");
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [errors, setErrors] = useState("");
+  const {register, handleSubmit, errors} = useForm({mode: "onChange"});
+  const onSubmit = submitInfo => console.log(submitInfo);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    setIsAuthenticated(Authentication.login(accountNumber));
-  };
-
-  const handleChange = e => {
-    setAccountNumber(e.target.value);
-    setErrors(Number.isNaN(e.target.value) ? "Input must be a number!" : "");
-  };
-
-  const TheLoginForm = (
-    <Form onSubmit={handleSubmit}>
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
       <Form.Group>
-        <Form.Label column>Account Number</Form.Label>
+        <Form.Label column={false}>Email</Form.Label>
         <Form.Control
-          type="text"
-          placeholder="Enter account number"
-          value={accountNumber}
-          onChange={handleChange}
+          name="email"
+          type="email"
+          placeholder="john@doe.com"
+          ref={register({
+            required: true,
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: "Please insert a valid email address!"
+            }
+          })}
         />
         <Form.Text className="text-danger">
-          {errors.length > 0 ? errors : <span>&nbsp;&nbsp;</span>}
+          {errors.email && errors.email.message} &nbsp;
         </Form.Text>
       </Form.Group>
-      <Button variant="primary" type="submit" block disabled={errors !== ""}>
+
+      <Form.Group>
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          name="password"
+          type="password"
+          placeholder="place here"
+          ref={register({required: true})}
+        />
+        <Form.Text className="text-danger">
+          {errors.password && "Please insert your password!"} &nbsp;
+        </Form.Text>
+      </Form.Group>
+
+      <Button variant="primary" type="submit" block className="mt-4">
         Login
       </Button>
+
+      <Form.Text className="text-center mt-4 font-weight-bold">
+        <span>Don't have an account? </span>
+        <Link to="/register" className="text-primary"> Register here </Link>
+      </Form.Text>
     </Form>
   );
-
-  return isAuthenticated ? <Redirect to="/home" /> : TheLoginForm;
 }
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <LoginForm/>
+    </>
+  )
 }
