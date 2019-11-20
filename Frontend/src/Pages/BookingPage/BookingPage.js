21da6167f0916a7ebb611d7ec3805decf347d01b
Ferdian Ifkarsyah <13517024@std.stei.itb.ac.id>
2019-11-21 01:38:59 +0700

    95% finish booking page

--- Frontend/src/Pages/BookingPage/BookingPage.js
+++ Frontend/src/Pages/BookingPage/BookingPage.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useLayoutEffect, useState} from "react";
 import {useLocation, useParams} from "react-router";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import {Engima} from "../../Utilities/Engima";
 import queryString from "query-string";
-import {Link} from "react-router-dom";
 import {Button} from "react-bootstrap";
-import FormLabel from "react-bootstrap/FormLabel";
+import {ScheduleHeader} from "./ScheduleHeader";
+import {func} from "prop-types";
 
 export default function BookingPage() {
   const {scheduleId} = useParams();
@@ -15,7 +15,9 @@ export default function BookingPage() {
   const urlQueries = queryString.parse(location.search);
   const [schedule, setSchedule] = useState({});
   const [seats, setSeats] = useState([]);
-  const [selectedSeats, setSelectedSeat] = useState(0);
+  const [loaded, setLoaded] = useState(false);
+  const [selected, setSelected] = useState(-1);
+  const [lastSelect, setLastSelect] = useState(-1);
 
   useEffect(() => {
     (async () => {
@@ -33,78 +35,108 @@ export default function BookingPage() {
 
       const seatInteger = parseInt(body['seats']);
       body['seats'] = seatInteger.toString(2);
+
       setSeats(Array.from(body['seats']));
       setSchedule(body);
+      setLoaded(true);
     })();
   }, []);
 
   const handleSubmit = () => {
-    alert(selectedSeats);
+    alert(selected);
   };
 
-  const handleSeatSelection = (num) => {
-    setSelectedSeat(num);
+  const handleClick = (num) => {
+    setSelected(num);
+    const newSeats = [...seats];
+    newSeats[num] = '2';
+    newSeats[lastSelect] = '1';
+    setSeats(newSeats);
+    setLastSelect(num);
+  };
+
+  if (!loaded) {
+    return <p>Loading....</p>
+  }
+
+  const Payment = () => {
+    if (selected === -1) {
+      return null;
+    }
+    return (
+      <>
+        <Col xs={4}><h6 className="font-weight-bold mb-4">Seat {selected}</h6></Col>
+        <Col xs={8} className="text-right">
+          <h6 className="font-weight-bold mb-4">Rp 45.000</h6>
+          <Button className="text-white" onClick={handleSubmit}>Buy Ticket</Button>
+        </Col>
+      </>
+    );
   };
 
   return (
     <Container fluid={true} className="mt-5">
-      <Row className="mb-5 mt-5">
-        <Col xs={1} className="d-flex flex-column justify-content-center">
-          <Link to={"/movies/" + schedule['movie_id']} style={{textDecoration: 'none'}}>
-            <span className="text-primary" style={{fontSize: '30px'}}>❮</span>
-          </Link>
-        </Col>
-        <Col xs={11} className="d-flex flex-column justify-content-center">
-          <h3 className="font-weight-bolder">{urlQueries['title']}</h3>
-          <p className="font-weight-bold text-secondary">{schedule['date'] + ' - ' + schedule['time']}</p>
-        </Col>
-      </Row>
-      <hr style={{border: '1px solid #d9d9d9'}}/>
-
+      <ScheduleHeader schedule={schedule} urlQueries={urlQueries}/>
       <Row>
         <Col xs={7} className="d-flex flex-wrap">
-          {seats.map((code, i) => <Seat num={i} code={code} handleSeatSelection={() => handleSeatSelection(i)}/>)}
+          <SeatList seatInformation={seats} handleClick={handleClick}/>
           <Button variant="secondary" block disabled>Screen</Button>
         </Col>
-
         <Col xs={5}>
           <h4 className="font-weight-bold text-secondary mb-4">Booking Summary</h4>
-
           <h5 className="font-weight-bold">{urlQueries['title']}</h5>
           <p className="font-weight-light text-secondary">{schedule['date'] + ' - ' + schedule['time']}</p>
           <Row>
-            <Col xs={4}>
-              <h6 className="font-weight-bold mb-4">Seat #4</h6>
-            </Col>
-            <Col xs={8} className="text-right">
-              <h6 className="font-weight-bold mb-4">Rp 45.000</h6>
-              <Button className="text-white" onSubmit={handleSubmit}>Buy Ticket</Button>
-            </Col>
+            <Payment/>
           </Row>
         </Col>
       </Row>
-
     </Container>
   );
 }
 
-function Seat({num, code, handleSeatSelection}) {
-  const style = {height: '50px', width: '50px', userSelect: 'none'};
-  const reservedButton = <Button className="m-1 " variant="secondary" style={style} disabled>{num}</Button>;
-
-  const selectedButton = <Button className="m-1 " variant="primary" style={style}
-                                 onClick={handleSeatSelection}>{num}</Button>;
-
-  const normalButton = <Button className="m-1 " variant="outline-primary" style={style}
-                               onClick={handleSeatSelection}>{num}</Button>;
-
-
-  switch (code) {
-    case '0':
-      return reservedButton;
-    case '1':
-      return normalButton;
-    case '2':
-      return selectedButton;
+const Seat = (props) => {
+  const style = {height: '50px', width: '50px'};
+  const {handleClick, num, status} = props;
+  const reserved = <Button style={style} className="m-1" onClick={handleClick} variant="secondary"
+                           disabled>{num}</Button>;
+  const normal = <Button style={style} className="m-1" onClick={handleClick} variant="outline-primary">{num}</Button>;
+  const selected = <Button style={style} className="m-1" onClick={handleClick}>{num}</Button>;
+  switch (parseInt(status)) {
+    case 0:
+      return reserved;
+    case 1:
+      return normal;
+    case 2:
+      return selected;
+    default:
+      return null;
   }
-}
+};
+
+const SeatList = ({seatInformation, handleClick}) => {
+  const [seatNow, setSeatNow] = useState(seatInformation);
+  const [selected, setSelected] = useState(-1);
+  const [lastSelect, setLastSelect] = useState(-1);
+
+  const handleChange = (idx) => {
+    setSeatNow(prevSeats => {
+      const newSeats = [...seatNow];
+      newSeats[lastSelect - 1] = '1';
+      newSeats[idx - 1] = '2';
+      return newSeats;
+    });
+    setSelected(idx);
+    setLastSelect(selected);
+  };
+
+  const rendered = seatNow.map((status, idx) => <Seat key={idx + 1} status={status} num={idx + 1} handleClick={() => {
+    handleClick(idx + 1);
+    handleChange(idx + 1);
+  }}/>)
+  return (
+    <div>
+      {rendered}
+    </div>
+  );
+};
